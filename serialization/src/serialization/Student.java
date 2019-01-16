666af890e21a5a139c82640d78eb0896fc83439d
Pranith Srujan Roy <ammumaddineni97@gmail.com>
2019-01-16 10:41:50 +0530

    Add files via upload

--- serialization/src/serialization/Student.java
+++ serialization/src/serialization/Student.java
@@ -7,7 +7,7 @@ package serialization;
 import java.io.*;
 /**
  *
- * @author Student
+ * @author Pranith Srujan Roy
  */
 public class Student implements java.io.Serializable{
     /**
@@ -19,78 +19,61 @@ public class Student implements java.io.Serializable{
         this.name = name;
         this.age = age;
     }
+    void serializeStudent(Student stu, String file){
+        try{
+            FileOutputStream f = new FileOutputStream(file);
+            ObjectOutputStream o = new ObjectOutputStream(f);
+            
+            o.writeObject(stu);
+            
+            o.close();
+            f.close();
+            System.out.println(stu.name + " object serialized");
+        }
+        catch(IOException e){
+            System.out.println("IOException caught");
+        }
+    }
+    
+    Student deSerializeStudent(String file){
+        Student stu = null;
+        try{
+            FileInputStream f = new FileInputStream(file);
+            ObjectInputStream i = new ObjectInputStream(f);
+            
+            stu = (Student)i.readObject();
+            
+            i.close();
+            f.close();
+            
+            System.out.println(stu.name + " object deserialized");
+        }
+        catch(Exception e){
+            System.out.println("Exception caught");
+        }
+        return stu;
+    }
+    
+    void printObject(Student stu){
+        System.out.println("Name: "+stu.name);
+        System.out.println("Age: "+stu.age);
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
         Student stu1 = new Student("Sita", 23);
         Student stu2 = new Student("Gita", 24);
         String stud1 = "stu1.txt", stud2 = "stu2.txt";
-        //Serializing stu1
-        try{
-            FileOutputStream file = new FileOutputStream(stud1); 
-            ObjectOutputStream out = new ObjectOutputStream(file); 
-              
-            // Method for serialization of object 
-            out.writeObject(stu1); 
-              
-            out.close(); 
-            file.close(); 
-              
-            System.out.println("Student1 has been serialized"); 
-        }
-        catch(IOException ex){
-            System.out.println("IOException is caught");
-        }
-        //Serializing stu2
-        try{
-            FileOutputStream file2 = new FileOutputStream(stud2);
-            ObjectOutputStream out2 = new ObjectOutputStream(file2);
-            //Serializing stu2 object
-            out2.writeObject(stu2);
-            out2.close();
-            file2.close();
-            
-            System.out.println("Student2 has been serialized");
-        }
-        catch(IOException ex){
-            System.out.println("IOException is caught");
-        }
-        
+        //Serializing Student objects
+        stu1.serializeStudent(stu1, stud1);
+        stu2.serializeStudent(stu2, stud2);
         //Deserializing
         Student st1 = null;
         Student st2 = null;
-        try{
-            FileInputStream f1 = new FileInputStream(stud1);
-            ObjectInputStream i1 = new ObjectInputStream(f1);
-            
-            st1 = (Student)i1.readObject();
-            
-            i1.close();
-            f1.close();
-            
-            System.out.println("Student1 has been deserialized");
-            System.out.println("Name: "+st1.name);
-            System.out.println("age: "+st1.age);
-        }
-        catch(Exception ex){
-            System.out.println("Exception caught while deserializing student1 object");
-        }
+        stu1 = stu1.deSerializeStudent(stud1);
+        stu1.printObject(stu1);
         
-        try{
-            FileInputStream f2 = new FileInputStream(stud2);
-            ObjectInputStream i2 = new ObjectInputStream(f2);
-            
-            st2 = (Student)i2.readObject();
-            
-            i2.close();
-            f2.close();
-            
-            System.out.println("Student2 has been deserialized");
-            System.out.println("Name: "+st2.name);
-            System.out.println("age: "+st2.age);
-        }
-        catch(Exception e){
-            System.out.println("Exception caught while deserializing student2");
-        }
+        stu2 = stu2.deSerializeStudent(stud2);
+        stu2.printObject(stu2);
     }
-    
 }
